0138bb8f5067a89d8b0420a3663049438bf908ec
Surajit05021997 <surajitmaity12345@gmail.com>
2021-05-18 17:26:17 +0530

    Added AppHeader for navigation

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
   <div id="app">
-
+    <app-header></app-header>
   </div>
 </template>
 
+<script>
+import AppHeader from '@/components/AppHeader.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+  },
+}
+</script>
+
 <style>
+html, body {
+  height: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="app-header">
+    <div class="nav">
+      <b-navbar toggleable="md" type="dark" variant="dark" fixed="top">
+        <router-link id="brand" to="/"><h4><b>TV MAZE</b></h4></router-link>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <router-link id="homeRoute" to="/">Home</router-link>
+            <router-link id="aboutRoute" to="/about">About</router-link>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+};
+</script>
+
+<style scoped>
+#brand {
+  color: rgb(221, 25, 25);
+}
+#brand h4 {
+  margin: 0px;
+}
+a {
+  color: grey;
+  margin: 4px;
+}
+a:hover {
+  color: lightgrey;
+  text-decoration: none;
+}
+.router-link-exact-active {
+  color: white;
+}
+</style>
