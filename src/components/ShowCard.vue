a72b924ff458442f81e1d00de0c5494737573bc7
Surajit05021997 <surajitmaity12345@gmail.com>
2021-05-18 19:34:31 +0530

    Added Show Card comp and top shows in home page

---
+++ src/components/ShowCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="show-card">
+    <router-link to="/">
+      <img class="show-image" :src="cardImage" alt="Show Image"><br>
+      <b>{{show.name}}</b><br>
+      Rating: {{rating || 'N/A'}}<br>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShowCard',
+  props: ['show'],
+  computed: {
+    rating() {
+      const [rating] = Object.values(this.show.rating);
+      return rating;
+    },
+    cardImage() {
+      if (this.show.image !== null) {
+        return this.show.image.medium;
+      }
+      return null;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.show-image {
+  width: 150px;
+}
+a {
+  color: black;
+  transition: color 250ms;
+}
+a:hover {
+  color: rgb(221, 25, 25);
+  text-decoration: none;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,39 @@
 <template>
   <div class="home">
-    
+    <div class="main-container" v-if="!isLoading && !error">
+      <div class="show-container">
+        <h4><b>Top Shows</b></h4>
+        <div class="shows">
+          <div class="show" v-for="show in getTop20Shows" :key="show.id">
+            <show-card :show="show"></show-card>
+          </div>
+        </div>
+      </div>  
+    </div>
+
+    <div v-else-if="!isLoading && error">
+    </div>
+
+    <div class="show-spinner" v-else>
+      <b-spinner variant="danger" label="Spinning"></b-spinner>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import ShowCard from '@/components/ShowCard.vue';
 
 export default {
   name: 'Home',
   components: {
-
+    ShowCard,
+  },
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+    };
   },
   created() {
     this.getAllShows();
@@ -19,9 +42,15 @@ export default {
     ...mapGetters(['getTop20Shows']),
   },
   methods: {
-    getAllShows() {
-      this.$store.dispatch('getAllShows');
-    }
+    async getAllShows() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('getAllShows');
+      } catch (error) {
+        this.error = error.message;
+      }
+      this.isLoading = false;
+    },
   },
 }
 </script>
@@ -31,5 +60,21 @@ export default {
   min-height: 100%;
   padding-top: 52.67px;
   padding-bottom: 24px;
+}
+.main-container {
+  margin: 20px;
+}
+.show-container {
+  margin: 20px 0px;
+}
+.shows {
+  display: flex;
+  overflow-y: auto;
+}
+.show {
+  margin: 10px;
+}
+.show-spinner {
+  margin: 40vh 0px;
 }
 </style>
