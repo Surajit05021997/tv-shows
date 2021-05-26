7b74cc2b4bcdcd77b4105a4c84c9953b819cf023
Surajit05021997 <surajitmaity12345@gmail.com>
2021-05-26 13:31:16 +0530

    Minor change

--- src/App.vue
+++ src/App.vue
@@ -27,8 +27,6 @@ html, body {
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   height: 100%;
 }
